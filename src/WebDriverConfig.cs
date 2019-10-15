8ee3dd41e86eed7b541b045d13c635beca89ec59
Rafael Brito <rafael.brito@bs2.com>
2019-10-15 12:05:01 -0300

    Projeto Teste Bluesoft

---
+++ src/WebDriverConfig.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+
+namespace TesteBlueSoft
+{
+    public class WebDriverConfig
+
+    {
+        public IWebDriver driver;
+        public WebDriverWait waiter;
+
+        public WebDriverConfig()
+        {
+            var chromeOptions = new ChromeOptions();
+            driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, chromeOptions);
+            driver.Manage().Window.Maximize();
+            waiter = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+    }
+}
